cb84aa29cbf09389e4bb37c9d2cdb9a0d23a6fba
Saadi <usenkanovsaadi@gmail.com>
2024-11-01 16:33:05 +0600

    Add files via upload

---
+++ lesson1/src/components/DZ1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dz1 = () => {
+    const content = "Дз 1 Saadi"
+    return (
+        <div>
+            {
+                content
+            }
+        </div>
+    );
+};
+
+export default Dz1;
---
+++ lesson1/src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = ({title}) => {
+    // console.log(props,'props')
+    // const {title} = props
+    return (
+        <div style={{color:'blue'}}>
+            {
+                title
+            }
+        </div>
+    );
+};
+
+export default Title;
